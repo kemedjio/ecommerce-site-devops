3d0c777681115ac5014d335d2ec68399c5bdd176
anicet <anicet@gmail.com>
2026-05-23 03:50:16 +0100

    main: modified file to trigger again CI

--- src/currencyservice/genproto.sh
+++ src/currencyservice/genproto.sh
@@ -7,7 +7,8 @@
 # You may obtain a copy of the License at
 # test line
 #      http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
+# test line
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
